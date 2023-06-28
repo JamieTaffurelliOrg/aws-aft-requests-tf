23591bdd1f38d2e2c276246c246b153a1300731f
Jamie Taffurelli <jltaffurelli@outlook.com>
2023-06-28 15:22:18 +0100

    added identity account

---
+++ terraform/accounts.tf
@@ -0,0 +1,31 @@
+module "identity" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "jamietaffurelli+aws-identity@jamietaffurelli.onmicrosoft.com"
+    AccountName               = "identity-01"
+    ManagedOrganizationalUnit = "Security"
+    SSOUserEmail              = "jamietaffurelli+aws-identity@jamietaffurelli.onmicrosoft.com"
+    SSOUserFirstName          = "Jamie"
+    SSOUserLastName           = "Taffurelli"
+  }
+
+  account_tags = {
+    data-classification = "confidential"
+    criticality         = "mission-critical"
+    ops-commitment      = "workload-operations"
+    ops-team            = "sre"
+    cost-owner          = "jltaffurelli@outlook.com"
+    owner               = "jltaffurelli@outlook.com"
+    sla                 = "high"
+    environment         = "prod"
+    stack               = "identity"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Jamie Taffurelli"
+    change_reason       = "Creating account for idenity related resources (e.g. OIDC providers)"
+  }
+
+  account_customizations_name = ""
+}
