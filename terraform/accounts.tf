d0411e6ce5a7f8deea1c1fb91dd8c672d0058138
Jamie Taffurelli <jltaffurelli@outlook.com>
2023-07-03 13:38:43 +0100

    try test 1

--- terraform/accounts.tf
+++ terraform/accounts.tf
@@ -29,3 +29,35 @@ module "testabc" {
 
   account_customizations_name = ""
 }
+
+module "testabc1" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "jamietaffurelli+awstestabc1@jamietaffurelli.onmicrosoft.com"
+    AccountName               = "testabc1"
+    ManagedOrganizationalUnit = "Identity (ou-kh9q-rd0xycel)"
+    SSOUserEmail              = "jamietaffurelli+awstestabc1@jamietaffurelli.onmicrosoft.com"
+    SSOUserFirstName          = "Jamie"
+    SSOUserLastName           = "Taffurelli"
+  }
+
+  account_tags = {
+    "data-classification" = "confidential"
+    "criticality"         = "mission-critical"
+    "ops-commitment"      = "workload-operations"
+    "ops-team"            = "sre"
+    "cost-owner"          = "jltaffurelli@outlook.com"
+    "owner"               = "jltaffurelli@outlook.com"
+    "sla"                 = "high"
+    "environment"         = "prod"
+    "stack"               = "identity"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Jamie Taffurelli"
+    change_reason       = "Creating account for idenity related resources (e.g. OIDC providers)"
+  }
+
+  account_customizations_name = ""
+}
