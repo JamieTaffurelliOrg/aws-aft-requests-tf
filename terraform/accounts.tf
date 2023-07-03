a9cdbb0ae645fdf20986b35c55b5f6dd05b68b23
Jamie Taffurelli <jltaffurelli@outlook.com>
2023-07-03 14:48:52 +0100

    added connectivity dev account

--- terraform/accounts.tf
+++ terraform/accounts.tf
@@ -1,35 +1,3 @@
-module "iden-shrd-01" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "jamietaffurelli+aws-identity-shared-01@jamietaffurelli.onmicrosoft.com"
-    AccountName               = "identity-shared-01"
-    ManagedOrganizationalUnit = "Identity (ou-kh9q-rd0xycel)"
-    SSOUserEmail              = "jamietaffurelli+aws-identity-shared-01@jamietaffurelli.onmicrosoft.com"
-    SSOUserFirstName          = "Jamie"
-    SSOUserLastName           = "Taffurelli"
-  }
-
-  account_tags = {
-    "data-classification" = "confidential"
-    "criticality"         = "mission-critical"
-    "ops-commitment"      = "workload-operations"
-    "ops-team"            = "sre"
-    "cost-owner"          = "jltaffurelli@outlook.com"
-    "owner"               = "jltaffurelli@outlook.com"
-    "sla"                 = "high"
-    "environment"         = "prod"
-    "stack"               = "identity"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "Jamie Taffurelli"
-    change_reason       = "Creating account for idenity related resources (e.g. OIDC providers)"
-  }
-
-  account_customizations_name = ""
-}
-
 module "idenshrd01" {
   source = "./modules/aft-account-request"
 
@@ -57,6 +25,38 @@ module "idenshrd01" {
   change_management_parameters = {
     change_requested_by = "Jamie Taffurelli"
     change_reason       = "Creating account for idenity related resources (e.g. OIDC providers)"
+  }
+
+  account_customizations_name = ""
+}
+
+module "conn-dev-01" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "jamietaffurelli+aws-conn-dev-01@jamietaffurelli.onmicrosoft.com"
+    AccountName               = "conn-dev-01"
+    ManagedOrganizationalUnit = "Connectivity (ou-kh9q-prwg8dbb)"
+    SSOUserEmail              = "jamietaffurelli+aws-conn-dev-01@jamietaffurelli.onmicrosoft.com"
+    SSOUserFirstName          = "Jamie"
+    SSOUserLastName           = "Taffurelli"
+  }
+
+  account_tags = {
+    "data-classification" = "confidential"
+    "criticality"         = "mission-critical"
+    "ops-commitment"      = "workload-operations"
+    "ops-team"            = "sre"
+    "cost-owner"          = "jltaffurelli@outlook.com"
+    "owner"               = "jltaffurelli@outlook.com"
+    "sla"                 = "high"
+    "environment"         = "prod"
+    "stack"               = "identity"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Jamie Taffurelli"
+    change_reason       = "Creating account for networking dev related resources"
   }
 
   account_customizations_name = ""
