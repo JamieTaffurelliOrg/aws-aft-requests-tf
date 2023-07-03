d97632eba885a739681950a11d4e8f91374bbc8a
Jamie Taffurelli <jltaffurelli@outlook.com>
2023-07-03 15:02:46 +0100

    added connectivity prod account

--- terraform/accounts.tf
+++ terraform/accounts.tf
@@ -50,13 +50,45 @@ module "conn-dev-01" {
     "cost-owner"          = "jltaffurelli@outlook.com"
     "owner"               = "jltaffurelli@outlook.com"
     "sla"                 = "high"
-    "environment"         = "prod"
-    "stack"               = "identity"
+    "environment"         = "dev"
+    "stack"               = "connectivity"
   }
 
   change_management_parameters = {
     change_requested_by = "Jamie Taffurelli"
     change_reason       = "Creating account for networking dev related resources"
+  }
+
+  account_customizations_name = ""
+}
+
+module "conn-prod-01" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "jamietaffurelli+aws-conn-prod-01@jamietaffurelli.onmicrosoft.com"
+    AccountName               = "conn-prod-01"
+    ManagedOrganizationalUnit = "Connectivity (ou-kh9q-prwg8dbb)"
+    SSOUserEmail              = "jamietaffurelli+aws-conn-prod-01@jamietaffurelli.onmicrosoft.com"
+    SSOUserFirstName          = "Jamie"
+    SSOUserLastName           = "Taffurelli"
+  }
+
+  account_tags = {
+    "data-classification" = "confidential"
+    "criticality"         = "mission-critical"
+    "ops-commitment"      = "workload-operations"
+    "ops-team"            = "sre"
+    "cost-owner"          = "jltaffurelli@outlook.com"
+    "owner"               = "jltaffurelli@outlook.com"
+    "sla"                 = "high"
+    "environment"         = "prod"
+    "stack"               = "connectivity"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Jamie Taffurelli"
+    change_reason       = "Creating account for networking prod related resources"
   }
 
   account_customizations_name = ""
