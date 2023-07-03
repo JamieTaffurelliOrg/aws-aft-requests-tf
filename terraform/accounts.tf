43d8115b9eef3fea7b4cafdd0869c8c952fde182
Jamie Taffurelli <jltaffurelli@outlook.com>
2023-07-03 15:36:11 +0100

    added tf external account

--- terraform/accounts.tf
+++ terraform/accounts.tf
@@ -1,3 +1,35 @@
+module "tf-external-01" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "jamietaffurelli+aws-tf-external-01@jamietaffurelli.onmicrosoft.com"
+    AccountName               = "tf-external-01"
+    ManagedOrganizationalUnit = "TF-External"
+    SSOUserEmail              = "jamietaffurelli+aws-tf-external-01@jamietaffurelli.onmicrosoft.com"
+    SSOUserFirstName          = "Jamie"
+    SSOUserLastName           = "Taffurelli"
+  }
+
+  account_tags = {
+    "data-classification" = "confidential"
+    "criticality"         = "mission-critical"
+    "ops-commitment"      = "workload-operations"
+    "ops-team"            = "sre"
+    "cost-owner"          = "jltaffurelli@outlook.com"
+    "owner"               = "jltaffurelli@outlook.com"
+    "sla"                 = "high"
+    "environment"         = "prod"
+    "stack"               = "terraform"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Jamie Taffurelli"
+    change_reason       = "Creating account for external services that have tf state"
+  }
+
+  account_customizations_name = ""
+}
+
 module "idenshrd01" {
   source = "./modules/aft-account-request"
 
