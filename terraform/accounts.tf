0a33b23d1804fcaf3a2788654fb61fe3d336a9a8
Jamie Taffurelli <jltaffurelli@outlook.com>
2023-07-03 13:53:19 +0100

    try again

--- terraform/accounts.tf
+++ terraform/accounts.tf
@@ -61,3 +61,35 @@ module "testabc1" {
 
   account_customizations_name = ""
 }
+
+module "iden-shrd-01" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "jamietaffurelli+aws-identity-shared-01@jamietaffurelli.onmicrosoft.com"
+    AccountName               = "identity-shared-01"
+    ManagedOrganizationalUnit = "Identity (ou-kh9q-rd0xycel)"
+    SSOUserEmail              = "jamietaffurelli+aws-identity-shared-01@jamietaffurelli.onmicrosoft.com"
+    SSOUserFirstName          = "Jamie"
+    SSOUserLastName           = "Taffurelli"
+  }
+
+  account_tags = {
+    "data-classification" = "confidential"
+    "criticality"         = "mission-critical"
+    "ops-commitment"      = "workload-operations"
+    "ops-team"            = "sre"
+    "cost-owner"          = "jltaffurelli@outlook.com"
+    "owner"               = "jltaffurelli@outlook.com"
+    "sla"                 = "high"
+    "environment"         = "prod"
+    "stack"               = "identity"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Jamie Taffurelli"
+    change_reason       = "Creating account for idenity related resources (e.g. OIDC providers)"
+  }
+
+  account_customizations_name = ""
+}
