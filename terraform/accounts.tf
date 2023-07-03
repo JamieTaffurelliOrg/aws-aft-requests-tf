122eabcf20c5d72de974133468bbb6ab17a3d1e5
Jamie Taffurelli <jltaffurelli@outlook.com>
2023-07-03 14:09:14 +0100

    tried no hyphen

--- terraform/accounts.tf
+++ terraform/accounts.tf
@@ -29,3 +29,35 @@ module "iden-shrd-01" {
 
   account_customizations_name = ""
 }
+
+module "idenshrd01" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "jamietaffurelli+awsidenshrd01@jamietaffurelli.onmicrosoft.com"
+    AccountName               = "idenshrd01"
+    ManagedOrganizationalUnit = "Identity (ou-kh9q-rd0xycel)"
+    SSOUserEmail              = "jamietaffurelli+awsidenshrd01@jamietaffurelli.onmicrosoft.com"
+    SSOUserFirstName          = "Jamie"
+    SSOUserLastName           = "Taffurelli"
+  }
+
+  account_tags = {
+    "data-classification" = "confidential"
+    "criticality"         = "mission-critical"
+    "ops-commitment"      = "workload-operations"
+    "ops-team"            = "sre"
+    "cost-owner"          = "jltaffurelli@outlook.com"
+    "owner"               = "jltaffurelli@outlook.com"
+    "sla"                 = "high"
+    "environment"         = "prod"
+    "stack"               = "identity"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Jamie Taffurelli"
+    change_reason       = "Creating account for idenity related resources (e.g. OIDC providers)"
+  }
+
+  account_customizations_name = ""
+}
